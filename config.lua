1f30e090acf9b05dd2630318d006993df1f20b73
Clippy <68430534+Clippy29@users.noreply.github.com>
2021-05-24 18:00:09 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,17 @@
+if GetCurrentResourceName() ~= "cDivers" then return end
+
+Divers = Divers or {}
+
+Divers.BlockWheel = true
+Divers.CanRagdoll = true
+Divers.StunnedEffect = true
+Divers.BlockCross = true
+Divers.BlockHeadShot = true
+Divers.CleanPolice = true
+Divers.DisableVehicleRewards = true
+Divers.NoReloadLife = true
+Divers.AntiHelmetMoto = true
+Divers.RadarOnlyVehicle = true
+Divers.AntiPickup = true
+Divers.AntiDriveBy = true
+Divers.MaxKmhDriveBy = 60
